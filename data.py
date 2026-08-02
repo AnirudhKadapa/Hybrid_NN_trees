dd5d99e9751f8ecb713bae383667afa9fe5495e9
ak <anirudhsuper5@gmail.com>
2026-08-02 19:23:53 -0400

    data updated for other datasets

--- data.py
+++ data.py
@@ -77,26 +77,81 @@ def process_helena(dataset: Bunch, cache_dir:Path):
         )
     print(f"Saved numpy and pt file in {file_path.resolve()}")
 
+def process_higgs(higgs_csv, cache_dir:Path):
+    data = np.loadtxt(higgs_csv, delimiter=",", dtype=np.float32)
+    X = data[:, 1:].astype(np.float32)
+    y = data[:, 0].astype(np.int64)
 
-def load_helena_data(cache_dir:Path):
-    filename = 'helena_train_test_val.pt'
-    file = Path(cache_dir/filename).resolve() 
-    
-    if not file.exists():
-        raise FileNotFoundError(
-            f"File does not exist in the {cache_dir} "
+    X_train_val, X_test, y_train_val, y_test = train_test_split(X, y, test_size=0.10, random_state=42, shuffle=True, stratify=y)
+    X_train, X_val, y_train, y_val = train_test_split(X_train_val, y_train_val, test_size=1 / 9, random_state=42, shuffle=True,stratify=y_train_val)
+
+    scaler = StandardScaler()
+    X_train = scaler.fit_transform(X_train).astype(np.float32)
+    X_val = scaler.transform(X_val).astype(np.float32)
+    X_test = scaler.transform(X_test).astype(np.float32)
+    file_path = cache_dir
+    file_path.mkdir(parents=True,exist_ok=True)
+    # np.savez(file_path / "higgs_train_test_val.npz", X_train=X_train,y_train=y_train,X_test=X_test,y_test=y_test,X_val=X_val,y_val=y_val)
+    torch.save({
+        "X_train": torch.from_numpy(X_train),
+        "y_train": torch.from_numpy(y_train),
+        "X_val": torch.from_numpy(X_val),
+        "y_val": torch.from_numpy(y_val),
+        "X_test": torch.from_numpy(X_test),
+        "y_test": torch.from_numpy(y_test),
+        },
+        file_path / "higgs_train_test_val.pt"
         )
+    print(f"Saved numpy and pt file in {file_path.resolve()}")
 
-    data = torch.load(file, map_location="cpu", weights_only=True)
+def process_epsilon(npz_path, cache_dir:Path):
+    d = np.load(npz_path)
+    X_train = d["X_train"].astype(np.float32)
+    y_train = d["y_train"].astype(np.int64)
+    X_val = d["X_val"].astype(np.float32)
+    y_val = d["y_val"].astype(np.int64)
+    X_test = d["X_test"].astype(np.float32)
+    y_test = d["y_test"].astype(np.int64)
 
-    return (
-        data["X_train"],
-        data["y_train"], 
-        data["X_val"], 
-        data["y_val"], 
-        data["X_test"],
-        data["y_test"]
+    scaler = StandardScaler()
+    X_train = scaler.fit_transform(X_train).astype(np.float32)
+    X_val = scaler.transform(X_val).astype(np.float32)
+    X_test = scaler.transform(X_test).astype(np.float32)
+
+    file_path = cache_dir
+    file_path.mkdir(parents=True,exist_ok=True)
+    torch.save({
+    "X_train": torch.from_numpy(X_train),
+    "y_train": torch.from_numpy(y_train),
+    "X_val": torch.from_numpy(X_val),
+    "y_val": torch.from_numpy(y_val),
+    "X_test": torch.from_numpy(X_test),
+    "y_test": torch.from_numpy(y_test),
+    },
+    file_path / "epsilon_train_test_val.pt"
     )
+    print(f"Saved numpy and pt file in {file_path.resolve()}")
+
+def process_adult(np_tr, np_te, np_val, cache_dir:Path):
+    tr, va, te = np.load(np_tr), np.load(np_val), np.load(np_te)
+    X_train, y_train = tr[:, 1:].astype(np.float32), tr[:, 0].astype(np.int64)
+    X_val, y_val = va[:, 1:].astype(np.float32), va[:, 0].astype(np.int64)
+    X_test, y_test = te[:, 1:].astype(np.float32), te[:, 0].astype(np.int64)
+
+    file_path = cache_dir
+    file_path.mkdir(parents=True,exist_ok=True)
+    torch.save({
+    "X_train": torch.from_numpy(X_train),
+    "y_train": torch.from_numpy(y_train),
+    "X_val": torch.from_numpy(X_val),
+    "y_val": torch.from_numpy(y_val),
+    "X_test": torch.from_numpy(X_test),
+    "y_test": torch.from_numpy(y_test),
+    },
+    file_path / "adult_train_test_val.pt"
+    )
+    print(f"Saved numpy and pt file in {file_path.resolve()}")
+
 
 def load_data(cache_dir: Path, filename):
     file = Path(cache_dir/filename).resolve() 
@@ -146,9 +201,35 @@ if __name__=="__main__":
         print("Processing Helena Dataset") 
         dataset = fetch_openml(data_id=41169, as_frame=False, parser="auto", data_home=helena_data)
         process_helena(dataset, helena_data)
-   
-   
-    
+
+    higgs_csv = "./dataset_cache/higgs/HIGGS.csv"
+    higgs_path = Path("./dataset_cache/higgs")
+    higgs_file = higgs_path / 'higgs_train_test_val.pt'
+
+    if higgs_file.exists():
+        print("Higgs cache exist")
+    else:
+        process_higgs(higgs_csv,higgs_path )
+
+    epsilon_path = Path("./dataset_cache/epsilon")
+    epsilon_file = epsilon_path / "epsilon_train_test_val.pt"
+    epsilon_npz = epsilon_path / 'epsilon_train_test_val.npz'
+    if epsilon_file.exists():
+        print("epsilon cache exist")
+    else:
+        process_epsilon(epsilon_npz, epsilon_path)
+
+    adultincome_path = Path("./dataset_cache/adult")
+    adult_file  = adultincome_path / 'adult_train_test_val.pt'
+    np_train = "./dataset_cache/adult/adult_train.npy"
+    np_test = "./dataset_cache/adult/adult_test.npy"
+    np_val = "./dataset_cache/adult/adult_val.npy"
+    if adult_file.exists():
+        print("adult cache exist")
+    else:
+        process_adult(np_train, np_test, np_val, adultincome_path)
+
+
 
 
 
