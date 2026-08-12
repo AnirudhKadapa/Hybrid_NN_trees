a0e467106f9a7257e7135c28c47bf892e812bd02
ak <anirudhsuper5@gmail.com>
2026-08-12 11:46:34 -0400

    updated runs for the other experiments

--- run.sh
+++ run.sh
@@ -3,7 +3,7 @@
 datasets=("higgs" "epsilon" "helena")
 
 for data in "${datasets[@]}"; do
-    python main.py --datasets $data --results best_params/$data
+    python main.py --dataset $data --results best_params/$data
 done
 
 echo "all runs complete"
