5bacc6c39bfe15ff60c2941bd06fa7292b0e9f7a
ak <anirudhsuper5@gmail.com>
2026-08-19 01:36:06 -0400

    updated model and added run script

--- run.sh
+++ run.sh
@@ -1,8 +1,11 @@
 #!/bin/bash
+set -e 
 
 datasets=("higgs" "epsilon" "helena")
 
 for data in "${datasets[@]}"; do
+    echo ""
+    echo "Starting experiment $data"
     python main.py --dataset $data --results best_params/$data
 done
 
